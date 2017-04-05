7c61b90911223feb040a795963075863842035a6
miguelm197 <miguelmax197@gmail.com>
2017-04-05 11:43:54 -0300

    asfas

--- SHOP/src/shopTesting/agregarPresupuesto.java
+++ SHOP/src/shopTesting/agregarPresupuesto.java
@@ -26,24 +26,27 @@ public class agregarPresupuesto extends ApiaAbstractClass {
 
 		Entity currEnt = this.getCurrentEntity();
 		String envDir = currEnt.getAttribute("SH_FINPRESUPUESTO_PRESUPUESTO_COMPRA_STR").getValuesAsString();
-		//String listo = currEnt.getAttribute("SH_LISTO_PRESUPUESTOS_STR").getValuesAsString();
+		// String listo =
+		// currEnt.getAttribute("SH_LISTO_PRESUPUESTOS_STR").getValuesAsString();
 
 		String iva = currEnt.getAttribute("SH_IVA_PRESUPUESTO_STR").getValueAsString();
 		String suma = currEnt.getAttribute("SH_MONTO_MAS_IVA_PRESUPUESTOS_STR").getValueAsString();
 
 		// if (listo.equals("false")) {
-			if (envDir.equals("false")) {
-				if (iva.equals("Sin IVA")) {
-					currEnt.getAttribute("SH_MONTO_PRESUPUESTO_COMPRA_STR").setValue(suma);
+		if (envDir.equals("false")) {
+			if (iva.equals("Sin IVA")) {
+				currEnt.getAttribute("SH_MONTO_PRESUPUESTO_COMPRA_STR").setValue(suma);
 
-				}
-				// Se obtiene los valores de los campos
-				String proveedor = currEnt.getAttribute("SH_PROVEEDOR_PRESUPUESTO_COMPRA_STR").getValueAsString();
-				String cumplimiento = currEnt.getAttribute("SH_CUMPLIMIENTO_PRESUPUESTO_COMPRA_STR").getValueAsString();
-				String comprobante = currEnt.getAttribute("SH_COMPROBANTE_PRESUPUESTO_COMPRA_STR").getValueAsString();
-				String monto = currEnt.getAttribute("SH_MONTO_PRESUPUESTO_COMPRA_STR").getValueAsString();
-				String moneda = currEnt.getAttribute("SH_MONEDA_PRESUPUESTO_COMPRA_STR").getValueAsString();
-				String comentario = currEnt.getAttribute("SH_COMENTARIO_PRESUPUESTO_COMPRA_STR").getValueAsString();
+			}
+			// Se obtiene los valores de los campos
+			String proveedor = currEnt.getAttribute("SH_PROVEEDOR_PRESUPUESTO_COMPRA_STR").getValueAsString();
+			String cumplimiento = currEnt.getAttribute("SH_CUMPLIMIENTO_PRESUPUESTO_COMPRA_STR").getValueAsString();
+			String comprobante = currEnt.getAttribute("SH_COMPROBANTE_PRESUPUESTO_COMPRA_STR").getValueAsString();
+			String monto = currEnt.getAttribute("SH_MONTO_PRESUPUESTO_COMPRA_STR").getValueAsString();
+			String moneda = currEnt.getAttribute("SH_MONEDA_PRESUPUESTO_COMPRA_STR").getValueAsString();
+			String comentario = currEnt.getAttribute("SH_COMENTARIO_PRESUPUESTO_COMPRA_STR").getValueAsString();
+
+			if (proveedor == "" && monto == "") {
 
 				String monedita;
 				if (moneda.equals("Pesos")) {
@@ -56,26 +59,15 @@ public class agregarPresupuesto extends ApiaAbstractClass {
 				double mon;
 				int cum;
 
-//				try {
-//					mon = Double.parseDouble(monto);
-//				} catch (Exception e) {
-//					bandera = false;
-//					throw new BusClassException("Ingreso de monto inválido");
-//				} 
-
 				try {
-					cum = Integer.parseInt(cumplimiento);
+					mon = Double.parseDouble(monto);
 				} catch (Exception e) {
 					bandera = false;
-					throw new BusClassException("Ingreso de cumplimiento inválido");
+					throw new BusClassException("Ingreso de monto inválido");
 				}
 
-				 if (mon < 0) {
+				if (mon < 0) {
 					throw new BusClassException("El monto debe ser mayor a 0");
-				}
-
-				if (cum < 0 || cum > 10) {
-					throw new BusClassException("El cumplimiento debe estar entre el 0 y el 10");
 				}
 
 				if (bandera == true) {
@@ -111,40 +103,40 @@ public class agregarPresupuesto extends ApiaAbstractClass {
 					currEnt.getAttribute("SH_IVA_PRESUPUESTO_STR").setValue("Con IVA");
 
 				}
-
-			} else {
-				Collection favs = this.getCurrentEntity().getAttribute("SH_FAVORITOADMIN_PRESUPUESTO_INFO_STR")
-						.getValues();
-				ArrayList favoritos = new ArrayList();
-				favoritos.addAll(favs);
-
-				int cont = 0;
-
-				for (int i = 0; i < favoritos.size(); i++) {
-					if (favoritos.get(i).toString().compareTo("true") == 0) {
-						cont = cont + 1;
-					}
-				}
-
-				if (cont == 0) {
-					throw new BusClassException("Debe seleccionar al menos 1 favorito");
-				}
-				String nomUser = this.getCurrentUser().getName();
-				String coment = currEnt.getAttribute("SH_COMENTARIODIR_PRESUPUESTO_STR").getValueAsString();
-
-				DateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-				Date fechaActual = new Date();
-				String FechaActS = formatoFecha.format(fechaActual);
-
-				String comentario = FechaActS + " - " + nomUser + ": " +  coment + "\n\n";
-
-				Collection chat = this.getCurrentEntity().getAttribute("SH_CHAT_STR").getValues();
-				chat.add(comentario);
-				currEnt.getAttribute("SH_CHAT_STR").setValues(chat);
-
-				currEnt.getAttribute("SH_ESTADO_APROBACION_COMPRA_STR").setValue("Avanzar");
-
 			}
+
+		} else {
+			Collection favs = this.getCurrentEntity().getAttribute("SH_FAVORITOADMIN_PRESUPUESTO_INFO_STR").getValues();
+			ArrayList favoritos = new ArrayList();
+			favoritos.addAll(favs);
+
+			int cont = 0;
+
+			for (int i = 0; i < favoritos.size(); i++) {
+				if (favoritos.get(i).toString().compareTo("true") == 0) {
+					cont = cont + 1;
+				}
+			}
+
+			if (cont == 0) {
+				throw new BusClassException("Debe seleccionar al menos 1 favorito");
+			}
+			String nomUser = this.getCurrentUser().getName();
+			String coment = currEnt.getAttribute("SH_COMENTARIODIR_PRESUPUESTO_STR").getValueAsString();
+
+			DateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			Date fechaActual = new Date();
+			String FechaActS = formatoFecha.format(fechaActual);
+
+			String comentario = FechaActS + " - " + nomUser + ": " + coment + "\n\n";
+
+			Collection chat = this.getCurrentEntity().getAttribute("SH_CHAT_STR").getValues();
+			chat.add(comentario);
+			currEnt.getAttribute("SH_CHAT_STR").setValues(chat);
+
+			currEnt.getAttribute("SH_ESTADO_APROBACION_COMPRA_STR").setValue("Avanzar");
+
+		}
 		// }
 		// currEnt.getAttribute("SH_LISTO_PRESUPUESTOS_STR").clear();
 	}
